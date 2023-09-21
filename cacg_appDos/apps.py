ff02508d354ea630050aa8160299f2b84a6f2526
Cristhofer <yout69482@gmail.com>
2023-09-21 08:46:59 -0400

    Primer Commit del proyecto

---
+++ cacg_appDos/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CacgAppdosConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'cacg_appDos'
---
+++ cacg_appUno/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CacgAppunoConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'cacg_appUno'

522ea48fb53d0aec23b289a98e9071bd9bacac49
Cristhofer <yout69482@gmail.com>
2023-09-21 09:25:59 -0400

    Commit listoo

--- cacg_appDos/views.py
+++ cacg_appDos/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def vistaUno(request):
+    return HttpResponse("<H1> Aplicacion DOS, vista UNO <H1> <H2> Aplicacion DOS, vista UNO <H2> <H3> Aplicacion DOS, vista UNO <H3> <H4> Aplicacion DOS, vista UNO <H4>")
+
+def vistaDos(request):
+    return HttpResponse("<H1> Aplicacion DOS, vista DOS <H1> <H2> Aplicacion DOS, vista DOS <H2> <H3> Aplicacion DOS, vista DOS <H3> <H4> Aplicacion DOS, vista DOS <H4>")
